e0a1c85cecdcb9f29d116e90448522e546911e49
chordfish-k <592229466@qq.com>
2025-01-11 11:54:42 +0800

    feat: Channel类使用回调

---
+++ 08_channel_callback/src/epoll_server.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <fcntl.h>
+#include <functional>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <netinet/tcp.h>
+#include <vector>
+
+#include "net/InetAddress.h"
+#include "net/Socket.h"
+#include "net/Epoll.h"
+#include "net/Channel.h"
+
+int main(int argc, const char** argv) {
+  
+  if (argc != 3) {
+    printf("Usage: ./EpollServer <ip> <port>\n");
+    printf("Example: ./EpollServer 127.0.0.1 5005\n\n");
+    return -1;
+  }
+
+  // 服务端socket
+  Socket serverSocket;
+  // 服务端的地址和协议
+  InetAddress serverAddr(argv[1], atoi(argv[2]));
+  serverSocket.SetReuseAddr(true);
+  serverSocket.SetReusePort(true);
+  serverSocket.SetKeepAlive(true);
+  serverSocket.SetTcpNoDelay(true);
+  
+  // 绑定并监听
+  serverSocket.Bind(serverAddr);
+  serverSocket.Listen();
+ 
+  Epoll ep;
+  Channel* serverChannel = new Channel(&ep, serverSocket.GetFd());
+  serverChannel->SetReadCallback(std::bind(&Channel::OnNewConnection, serverChannel, &serverSocket));
+  serverChannel->EnableReading();
+
+  while (true) {
+    // 获取有事件的Channel
+    std::vector<Channel*> channels = ep.Loop();
+
+    for (auto& ch : channels) {
+      // 处理Channel事件
+      ch->HandleEvent();
+    }
+  }
+
+  return 0;
+}
---
+++ 08_channel_callback/src/net/Epoll.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Channel.h"
+
+#include <sys/epoll.h>
+#include <vector>
+
+class Epoll
+{
+private:
+  static const int s_MaxEvents = 100; // epoll_wait()返回事件数组的大小
+  int m_EpollFd = -1;                 // epoll句柄，再构造函数中创建
+  epoll_event m_Events[s_MaxEvents];  // 存放epoll_wait()返回事件的数组
+public:
+  Epoll();
+  ~Epoll();
+
+  // void AddFd(int fd, uint32_t op);                  // 把要监视的fd和事件添加到epoll红黑树
+  void UpdateChannel(Channel* ch);                  // 把channel添加/更新到红黑树上，channel中有fd和需要监视的事件
+  std::vector<Channel*> Loop(int timeout = -1);  // 运行epoll_wait(),等待事件发生，返回已发生的事件
+};
