ec7f7ccaa2058ab84bbae76352d90b3224609f8e
chordfish-k <592229466@qq.com>
2025-01-20 14:40:42 +0800

    init: 21

---
+++ 21_thread_pool/src/net/EchoServer.cpp
@@ -0,0 +1,56 @@
+#include "EchoServer.h"
+
+#include <functional>
+#include <iostream>
+
+EchoServer::EchoServer(const std::string& ip, const uint16_t port) 
+  : m_TcpServer(ip, port) {
+    m_TcpServer.SetNewConnectionCallback(std::bind(&EchoServer::OnNewConnection, this, std::placeholders::_1));
+    m_TcpServer.SetCloseConnectionCallback(std::bind(&EchoServer::OnCloseConnection, this, std::placeholders::_1));
+    m_TcpServer.SetErrorConnectionCallback(std::bind(&EchoServer::OnErrorConnection, this, std::placeholders::_1));
+    m_TcpServer.SetMessageCallback(std::bind(&EchoServer::OnMessage, this, std::placeholders::_1, std::placeholders::_2));
+    m_TcpServer.SetSendCompleteCallback(std::bind(&EchoServer::OnSendComplete, this, std::placeholders::_1));
+    // m_TcpServer.SetEpollTimeoutCallback(std::bind(&EchoServer::OnEpollTimeout, this, std::placeholders::_1));
+}
+
+EchoServer::~EchoServer() {
+
+}
+
+void EchoServer::Start() {
+  m_TcpServer.Start();
+}
+
+void EchoServer::OnNewConnection(Connection* clientSocket) {
+  std::cout << "New connection come in.\n";
+}
+
+void EchoServer::OnCloseConnection(Connection* conn) {
+  std::cout << "EchoServer conn error.\n";
+}
+
+void EchoServer::OnErrorConnection(Connection* conn) {
+
+}
+
+void EchoServer::OnMessage(Connection* conn, std::string& message) {
+  // 假设经过若干处理，得到结果
+  message = "Reply: " + message;
+  
+  // 该部分处理已添加在Buffer类中
+  // // 处理响应报文
+  // int len = message.size();
+  // std::string tmpBuf((char*)&len, 4); // 把报文头部填充到报文
+  // tmpBuf.append(message);
+
+  //send(conn->GetFd(), tmpBuf.data(), tmpBuf.size(), 0);
+  conn->Send(message.data(), message.size());
+}
+
+void EchoServer::OnSendComplete(Connection* conn) {
+  std::cout << "Message send complete.\n";
+}
+
+// void EchoServer::OnEpollTimeout(EventLoop* loop) {
+//   std::cout << "EchoServer timeout.\n";
+// }
---
+++ 21_thread_pool/src/net/TcpServer.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Connection.h"
+#include "EventLoop.h"
+#include "Channel.h"
+#include "Acceptor.h"
+
+#include <functional>
+#include <map>
+
+class TcpServer
+{
+private:
+  EventLoop m_Loop;     // 一个TcpServer可以有多个事件循环，单线程暂时只用一个事件循环
+  Acceptor* m_Acceptor; // 一个TcpServer只有一个Acceptor对象
+  std::map<int, Connection*> m_Conns; // 一个TcpServer有多个Connection对象，存放在map容器中
+  std::function<void(Connection*)> m_NewConnectionCallback;         // 回调 EchoServer 中对应函数
+  std::function<void(Connection*)> m_CloseConnectionCallback;       // 回调 EchoServer 中对应函数
+  std::function<void(Connection*)> m_ErrorConnectionCallback;       // 回调 EchoServer 中对应函数
+  std::function<void(Connection*, std::string&)> m_MessageCallback;  // 回调 EchoServer 中对应函数
+  std::function<void(Connection*)> m_SendCompleteCallback;          // 回调 EchoServer 中对应函数
+  std::function<void(EventLoop*)> m_EpollTimeoutCallback;           // 回调 EchoServer 中对应函数
+public:
+  TcpServer(const std::string& ip, const uint16_t port);
+  ~TcpServer();
+
+  void Start();      // 开启事件循环
+  
+  void OnNewConnection(Socket* clientSocket);   // 处理新的连接请求，在Acceptor类中回调该函数
+  void OnCloseConnection(Connection* conn);     // 关闭客户端的连接，在Connection中回调该函数
+  void OnErrorConnection(Connection* conn);     // 客户端的连接错误，在Connection中回调该函数
+  void OnMessage(Connection* conn, std::string& message);  // 处理客户端的请求报文，在Connection类中回调该函数
+  void OnSendComplete(Connection* conn);        // 数据发送完成后，在Connection类中回调该函数
+  void OnEpollTimeout(EventLoop* loop);         // epoll_wait超时，在EevntLoop类中回调
+
+  void SetNewConnectionCallback(std::function<void(Connection*)> fn);
+  void SetCloseConnectionCallback(std::function<void(Connection*)> fn);
+  void SetErrorConnectionCallback(std::function<void(Connection*)> fn);
+  void SetMessageCallback(std::function<void(Connection*, std::string&)> fn);
+  void SetSendCompleteCallback(std::function<void(Connection*)> fn);
+  void SetEpollTimeoutCallback(std::function<void(EventLoop*)> fn);
+};
