f97f55f6ffb4f4736083e8e6c7dacf6004b57770
chordfish-k <592229466@qq.com>
2025-01-22 20:11:23 +0800

    init: 25

---
+++ 25_unique_ptr/src/net/EventLoop.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Epoll.h"
+
+#include <functional>
+
+class Channel;
+class Epoll;
+
+// 事件循环类
+class EventLoop
+{
+private:
+  Epoll* m_Ep;   // 每个事件循环只有一个Epoll
+  std::function<void(EventLoop*)> m_EpollTimeoutCallback = nullptr;
+public:
+  EventLoop();  // 创建m_Ep
+  ~EventLoop(); // 析构函数，销毁m_Ep
+
+  void Run();
+  Epoll* GetEpoll() const;
+
+  void UpdateChannel(Channel* ch);  // 把Channel添加/更新到红黑树上，channel中有fd和需要监视的事件
+  void RemoveChannel(Channel* ch);  // 从epoll红黑树上删除Channel
+  void SetEpollTimeoutCallback(std::function<void(EventLoop*)> fn); 
+};
